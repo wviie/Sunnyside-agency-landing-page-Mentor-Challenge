1f0dd67f94dd3385fb7dd47f98723e3602c30708
Wviie <waviieca@gmail.com>
2024-04-05 01:48:41 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const navButton = document.querySelector('.humburger-button');
+const nav = document.querySelector('.nav');
+
+navButton.addEventListener('click', () => {
+    if(!(nav.style.display === 'flex')) {
+        nav.style.display = 'flex';
+    }
+    else {
+        nav.style.display = 'none';
+    }
+})
